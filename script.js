dd95131ff795339ac808e4856df3af7acb2181dd
googlermridul <goooglermridul@gmail.com>
2021-09-19 19:22:47 +0600

    search number show kora baki

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const getBook = () => {
    const inputField = document.getElementById("inputField")
    const inputValue = inputField.value;
 
+   // document.getElementById("loader").style.display = "block";
+   // document.getElementById("bookBox").style.display = "none";
+
    fetch(`https://openlibrary.org/search.json?q=${inputValue}`)
    .then(res => res.json())
    .then(data => {
@@ -14,27 +17,38 @@ const getBook = () => {
 const displayBooks = (books) => {
    const bookContainer = document.getElementById("bookContainer");
    bookContainer.innerHTML = '';
-   // console.log(books.length);
+   console.log(books.length);
    
    books.forEach(book => {
       console.log(book);
+      const {title, subtitle, author_name, first_publish_year, cover_i, publisher} = book;
 
       const div = document.createElement("div");
-      div.classList = "col-6 col-md-4 col-lg-3"
+      div.classList = "col-sm-6 col-lg-4"
       div.innerHTML = `
-         <div class="book-box">
-            <img class="book-img" src="${`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}" alt="">
-            <h5>${book.title}</h5>
+         <div id="bookBox" class="book-box">
+            <img class="book-img" src="${`https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`}" alt="">
+            <div class="book-info">
+               <h5 class="title">${title}</h5>
+               <p>Publish year: ${first_publish_year}</p>
+               <p class="mb-0">Publisher: ${publisher}</p>
+            </div>
          </div>
       `;
       bookContainer.appendChild(div)
    });
+   // document.getElementById("loader").style.display = "none";
+   // document.getElementById("bookBox").style.display = "block";
 }
 
-// 
-// title
-// subtitle
-// author_name
-// first_publish_year
-// cover_i
-// publisher
+
+// const toggleLoader = style => {
+//    document.getElementById("loader").style.display = style
+// }
+
+
+document.getElementById("inputField").addEventListener("keypress", function(event) {
+   if (event.key === "Enter") {
+      document.getElementById("searchBtn").click();
+   }
+})
