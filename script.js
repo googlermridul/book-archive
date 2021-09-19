b91c8e200dd63a966b7c8219a6d09885e1d59ff4
googlermridul <goooglermridul@gmail.com>
2021-09-19 20:03:16 +0600

    show founded data

--- script.js
+++ script.js
@@ -2,26 +2,29 @@ const getBook = () => {
    const inputField = document.getElementById("inputField")
    const inputValue = inputField.value;
 
-   // document.getElementById("loader").style.display = "block";
-   // document.getElementById("bookBox").style.display = "none";
+   if (inputValue === '') {
+      console.log("plese write something to display");      
+   }
 
    fetch(`https://openlibrary.org/search.json?q=${inputValue}`)
    .then(res => res.json())
    .then(data => {
-      displayBooks(data.docs)
-      console.log(data.numFound);
+      displayBooks(data)
    })
 }
 
 
-const displayBooks = (books) => {
+const displayBooks = (data) => {
    const bookContainer = document.getElementById("bookContainer");
    bookContainer.innerHTML = '';
-   console.log(books.length);
+
+   const books = data.docs;
    
    books.forEach(book => {
-      console.log(book);
-      const {title, subtitle, author_name, first_publish_year, cover_i, publisher} = book;
+      const {title, author_name, first_publish_year, cover_i, publisher} = book;
+
+      const foundedData = document.createElement('p');
+      foundedData.innerHTML = `<p>${books.length} results shown from ${data.numFound}</p>`
 
       const div = document.createElement("div");
       div.classList = "col-sm-6 col-lg-4"
@@ -30,21 +33,17 @@ const displayBooks = (books) => {
             <img class="book-img" src="${`https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`}" alt="">
             <div class="book-info">
                <h5 class="title">${title}</h5>
+               <p>By: ${author_name}</p>
                <p>Publish year: ${first_publish_year}</p>
                <p class="mb-0">Publisher: ${publisher}</p>
             </div>
          </div>
       `;
+      bookContainer.appendChild(foundedData)
       bookContainer.appendChild(div)
    });
-   // document.getElementById("loader").style.display = "none";
-   // document.getElementById("bookBox").style.display = "block";
+   
 }
-
-
-// const toggleLoader = style => {
-//    document.getElementById("loader").style.display = style
-// }
 
 
 document.getElementById("inputField").addEventListener("keypress", function(event) {
